1f83b0b59a968bd627cf25a0b3c91379009aaf7e
Pavel Glukhov <pavel.glukhov@t-systems.com>
2022-08-27 13:18:39 +0300

    [7] ex1

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -3,6 +3,14 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class SortableTable {
+  element;
+  data = [];
+  loading = false;
+  step = 20;
+  start = 1;
+  end = this.start + this.step;
+  subElements = {};
+
   constructor(headersConfig, {
     data = [],
     sorted = {}
@@ -10,11 +18,34 @@ export default class SortableTable {
 
   }
 
-  sortOnClient (id, order) {
+  onWindowScroll = async () => {
+    const {bottom} = this.element.getBoundingClientRect();
+    const {id, order} = this.sorted;
+
+    if (bottom < document.documentElement.clientHeight && !this.loading && !this.isSortLocally) {
+      this.start = this.end;
+      this.end = this.start + this.step;
+      this.loading = true;
+
+      const data = await this.loadData(id, order, this.start, this.end);
+      this.update(data);
+      this.loading = false;
+
+    }
+  };
+
+  sortOnClient(id, order) {
 
   }
 
-  sortOnServer (id, order) {
+  sortOnServer(id, order) {
 
+  }
+
+  initEventListeners() {
+     this.subElements.header.addEventListener('pointerdown', this.onSortClick);
+     window.addEventListener('scroll', this.onWindowScroll);
   }
 }
+
+
